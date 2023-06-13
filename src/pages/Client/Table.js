a6c3ae4a4415b5c918950d8d87a760eb6d06aa92
Viktor Nikolov <viktor.nickolov@gmail.com>
2023-06-13 19:26:24 +0300

    AddButton

--- src/pages/Client/Table.js
+++ src/pages/Client/Table.js
@@ -2,40 +2,41 @@ import React from 'react';
 import Button from "./AddButton";
 
 const Table = ({data}) => (
-  <div className="card">
-    <div className="header">
-      <h4 className="title">Striped Table with Hover</h4>
-      <p className="category">Here is a subtitle for this table</p>
-    </div>
-    <div className="content table-responsive table-full-width">
-        <div className="text-right" >
+    <div className="card">
+        <div className="header">
+            <h4 className="title">Striped Table with Hover</h4>
+            <p className="category">Here is a subtitle for this table</p>
+        </div>
+        <div className="text-right">
             <Button/>
         </div>
-      <table className="table table-hover table-striped">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Salary</th>
-            <th>Country</th>
-            <th>City</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(item => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.name}</td>
-              <td>$ {item.salary}</td>
-              <td>{item.country}</td>
-              <td>{item.city}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+        <div className="content table-responsive table-full-width">
 
+            <table className="table table-hover table-striped">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Salary</th>
+                    <th>Country</th>
+                    <th>City</th>
+                </tr>
+                </thead>
+                <tbody>
+                {data.map(item => (
+                    <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.name}</td>
+                        <td>$ {item.salary}</td>
+                        <td>{item.country}</td>
+                        <td>{item.city}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+
+        </div>
     </div>
-  </div>
 );
 
 export default Table;
